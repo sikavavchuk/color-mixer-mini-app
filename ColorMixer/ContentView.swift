56075815a7d49b2bbeac69352b95b55a3446c89d
Viktoriia <savchukv2006@gmail.com>
2026-04-21 20:44:16 +0200

    UI improvements

--- ColorMixer/ContentView.swift
+++ ColorMixer/ContentView.swift
@@ -10,21 +10,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color.mint)
-                .frame(height:200)
-                .padding(20)
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.mint)
+                    .frame(height:200)
+                    .padding(20)
+                Text("Color Preview")
+                    .font(Font.title.bold())
+                    .foregroundStyle(.white)
+            }
             VStack {
                 HStack {
                     Text("Red")
+                        .foregroundStyle(.red)
+                    Slider(value: .constant(0.5))
+                    Text("0.63")
                 }
                 HStack {
                     Text("Green")
+                        .foregroundStyle(.green)
+                    Slider(value: .constant(0.5))
+                    Text("0.63")
                 }
                 HStack {
                     Text("Blue")
+                        .foregroundStyle(.blue)
+                    Slider(value: .constant(0.5))
+                    Text("0.63")
                 }
             }
+            .padding()
             
         }
     }
