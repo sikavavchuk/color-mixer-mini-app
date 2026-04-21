1ed9ce54f253c80431c867e2be1a965c4fb0c1ac
Viktoriia <savchukv2006@gmail.com>
2026-04-21 21:00:49 +0200

    Code refactoring

--- ColorMixer/ContentView.swift
+++ ColorMixer/ContentView.swift
@@ -9,42 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            ZStack {
-                RoundedRectangle(cornerRadius: 20)
-                    .fill(Color.mint)
-                    .frame(height:200)
-                    .padding(20)
-                Text("Color Preview")
-                    .font(Font.title.bold())
-                    .foregroundStyle(.white)
-            }
-            VStack {
-                HStack {
-                    Text("Red")
-                        .foregroundStyle(.red)
-                    Slider(value: .constant(0.5))
-                    Text("0.63")
-                }
-                HStack {
-                    Text("Green")
-                        .foregroundStyle(.green)
-                    Slider(value: .constant(0.5))
-                    Text("0.63")
-                }
-                HStack {
-                    Text("Blue")
-                        .foregroundStyle(.blue)
-                    Slider(value: .constant(0.5))
-                    Text("0.63")
-                }
-            }
-            .padding()
-            
-        }
+        
+        
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct ColorMixerApplication: View {
+    @State private var red: Double = 0.0
+    @State private var green: Double = 0.0
+    @State private var blue: Double = 0.0
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color(red: red, green: green, blue: blue))
+                    .frame(height:200)
+                    .padding(20)
+                Text("Color Preview")
+                    .foregroundStyle(.white)
+                    .bold()
+                    .shadow(radius: 5)
+            }
+            VStack(spacing: 20) {
+                //sliders
+            }
+        }
+    }
 }
