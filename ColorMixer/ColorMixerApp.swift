02a1ec072f191dd943cbbe59c21fc3567370477d
Viktoriia <savchukv2006@gmail.com>
2026-04-21 20:28:51 +0200

    Synchronizing local repository with GitHub

---
+++ ColorMixer/ColorMixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixerApp.swift
+//  ColorMixer
+//
+//  Created by Viktoriia Savchuk  on 21/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
